a4fc4c4ca83477db32c91130af1f5adfeade14e5
Erick Garcia <erickgegarcia@gmail.com>
2023-12-19 14:34:26 -0300

    utilizando react-hook-form

--- src/app/assets/pages/homePage/components/aside-content/menu-dropDown/index.tsx
+++ src/app/assets/pages/homePage/components/aside-content/menu-dropDown/index.tsx
@@ -21,9 +21,6 @@ export function MenuDropdown({ title, list, height }: iAsideContent) {
         setActive((state) => !state)
     }
     const rotationClass = active ? 'rotate-0' : 'rotate-[-90deg]'
-    const dropdownStyle = active
-        ? 'transition-all duration-300 ease-out opacity-100 transform translate-y-0'
-        : 'transition-all duration-300 ease-in opacity-0 transform -translate-y-2'
 
     return (
         <>
@@ -33,7 +30,7 @@ export function MenuDropdown({ title, list, height }: iAsideContent) {
             </article>
             {
                 active === true ?
-                    < ul className={`w-[267px] h-fit rounded-[32px] bg-custom-gray-2 flex flex-col items-center justify-center border border-gray-900 gap-8 p-2`}>
+                    < ul className='w-[267px] h-fit rounded-[32px] bg-custom-gray-2 flex flex-col items-center justify-center border border-gray-900 gap-8 p-2 '>
                         {list.map((item) => (
                             <li key={item.name} className='w-48 h-14 p-4 rounded-[32px] hover:bg-custom-gray-3'>
                                 <a href="#" className='flex gap-4 flex-nowrap text-[14px]'>
--- src/app/assets/pages/homePage/components/newsLetter/index.tsx
+++ src/app/assets/pages/homePage/components/newsLetter/index.tsx
@@ -1,4 +1,41 @@
+'use client'
+
+import * as zod from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const newNewsLetterSubscriptionSchema = zod.object({
+    name: zod.string().min(10),
+    email: zod.string().min(10),
+    cellphone: zod.string().min(11).max(11)
+})
+
+type newNewsLetterSubscriptionData = zod.infer<typeof newNewsLetterSubscriptionSchema>
+
 export function NewsLetter() {
+
+    const { register, handleSubmit, watch, reset } = useForm<newNewsLetterSubscriptionData>({
+        resolver: zodResolver(newNewsLetterSubscriptionSchema),
+        defaultValues: {
+            name: '',
+            email: '',
+            cellphone: '',
+        }
+    })
+
+    const inputName = watch('name')
+    const inputEmail = watch('email')
+    const inputCellphone = watch('cellphone')
+
+    const isButtonDisabled = !(inputName && inputEmail && inputCellphone)
+
+    const cursorType = isButtonDisabled ? 'cursor-not-allowed' : 'cursor-pointer'
+
+    function assignNewsLetter(data: newNewsLetterSubscriptionData) {
+        console.log(data)
+        reset()
+    }
+
     return (
         <section className="flex justify-between gap-[122px] items-start mx-[120px] my-32">
             <div>
@@ -6,11 +43,57 @@ export function NewsLetter() {
                 <p className='w-[368px] text-[18px] mt-2 text-gray-200 font-bold'>Fique ligado no que tem de melhor no Mercado Financeiro.</p>
             </div>
             <div className='w-[564px]'>
-                <form action="" className="bg-custom-gray-3 rounded-[32px] flex flex-col items-center w-[458px] gap-4 py-[60px] px-[53px] text-gray-700">
-                    <input placeholder='Nome' type="text" name="" id="" className='w-[364px] rounded-[4px] bg-custom-gray p-1.5' />
-                    <input placeholder='Seu melhor e-mail' type="text" name="" id="" className='w-[364px] rounded-[4px] bg-custom-gray p-1.5' />
-                    <input placeholder='Celular' type="text" name="" id="" className='w-[364px] rounded-[4px]  bg-custom-gray p-1.5' />
-                    <button className='rounded-[4px] bg-custom-green-2 w-[364px] font-semibold text-black px-4 py-2.5'>Quero me inscrever</button>
+
+                <form onSubmit={handleSubmit(assignNewsLetter)} className="bg-custom-gray-3 rounded-[32px] flex flex-col items-center w-[458px] gap-4 py-[60px] px-[53px] text-gray-300">
+                    <input
+                        placeholder='Nome'
+                        type="text"
+                        id="name"
+                        {...register('name')}
+                        className='w-[364px] 
+                                rounded-[4px] 
+                                bg-custom-gray 
+                                p-1.5'
+                        minLength={10}
+                    />
+
+                    <input
+                        placeholder='Seu melhor e-mail'
+                        type="text"
+                        id="email"
+                        {...register('email')}
+                        className='w-[364px] 
+                            rounded-[4px] 
+                            bg-custom-gray 
+                            p-1.5'
+                        minLength={10}
+                    />
+
+                    <input
+                        placeholder='Celular'
+                        type="text"
+                        id="cellphone"
+                        {...register('cellphone')}
+                        className='w-[364px] 
+                            rounded-[4px]  
+                            bg-custom-gray p-1.5'
+                        minLength={11}
+                        maxLength={11}
+                    />
+                    <button
+                        className={`rounded-[4px] 
+                        bg-custom-green-2 
+                        w-[364px] 
+                        font-semibold 
+                        text-black 
+                        px-4 py-2.5
+                        ${cursorType}
+                        `}
+                        disabled={isButtonDisabled}
+                    >
+                        Quero me inscrever
+                    </button>
+
                 </form>
             </div>
         </section >
